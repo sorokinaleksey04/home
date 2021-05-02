6d1c3b1298e136d617de504bc6caf46a0494c27a
sorokinaleksey <sorokinaleksey04@gmail.com>
2021-05-02 19:34:21 +0300

    homeWork_11

---
+++ src/homeWork_11/RunUser.java
@@ -0,0 +1,16 @@
+package homeWork_11;
+
+public class RunUser {
+    public static void main(String[] args) {
+        User user = new User("Вася", 43);
+
+        System.out.println(user);
+
+        String text = """
+                       11111111
+                       2222222
+                       3333333
+                       """;
+        System.out.println(text);
+    }
+}
---
+++ src/homeWork_11/User.java
@@ -0,0 +1,5 @@
+package homeWork_11;
+
+public record User(String name, int age) {
+
+}
